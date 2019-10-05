0dba8f4829626ab2b3bdeab7f46a1620295b1ca3
John Yu <39107518+johnyu763@users.noreply.github.com>
2019-10-05 08:59:11 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-class StopWatch{
-  constructor(){
-    this.start_time = new Date().getTime();
+var startButton = document.querySelector('.startWatch');
+var pauseButton = document.querySelector('.pauseWatch');
+var resetButton = document.querySelector('.resetWatch');
+var historyTable = document.querySelector('table');
+var startTime;
+var running = 0;
+
+function startStopWatch(){
+  if(!running){
+    startTime = new Date().getTime();
+    running = 1;
   }
 }
+function pauseStopWatch(){
+  
+}
+function resetStopWatch(){
+  
+}
+
+
