372c4fd941d6db01de975a274db1974dc326bd2b
John Yu <39107518+johnyu763@users.noreply.github.com>
2019-10-06 11:12:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-var startButton = document.querySelector(".startWatch");
-var pauseButton = document.querySelector(".pauseWatch");
-var resetButton = document.querySelector(".resetWatch");
 var historyTable = document.querySelector("table");
 var tableSize = 0;
 var timeDisplay = document.querySelector(".timer");
@@ -17,17 +14,18 @@ var newStop;
 var stopLatitude;
 var stopLongitude;
 var elapsedTime;
-var hours;
-var minutes;
-var seconds;
-var centiseconds;
+var hours = 0;
+var minutes = 0;
+var seconds = 0;
+var centiseconds = 0;
 
 
 function startStopWatch() {
   if (!running) {
     startTime = new Date().getTime();
-    newRow = historyTable.insertRow(tableSize + 1);
     tableSize++;
+    newRow = historyTable.insertRow(tableSize);
+    
     newStart = newRow.insertCell(0);
     startLatitude = newRow.insertCell(1);
     startLongitude = newRow.insertCell(2);
@@ -45,8 +43,8 @@ function startStopWatch() {
     timeInterval = setInterval(getDisplayTime, 10);
     running = 1;
     paused = 0;
+    saveToLocalStorage();
   }
-  saveToLocalStorage();
 }
 function pauseStopWatch() {
   if (running && !paused) {
@@ -64,8 +62,8 @@ function pauseStopWatch() {
     }
     elapsedTime.innerHTML =
       hours + ":" + minutes + ":" + seconds + ":" + centiseconds;
+    saveToLocalStorage();
   }
-  saveToLocalStorage();
 }
 function resetStopWatch() {
   while (tableSize) {
@@ -78,7 +76,7 @@ function resetStopWatch() {
     running = 0;
     paused = 0;
   }
-  saveToLocalStorage();
+  localStorage.clear();
 }
 
 function getDisplayTime() {
@@ -105,38 +103,51 @@ function getFormattedTime() {
 function startCoor(pos) {
   startLatitude.innerHTML = pos.coords.latitude.toFixed(2);
   startLongitude.innerHTML = pos.coords.longitude.toFixed(2);
+  saveToLocalStorage();
 }
 
 function stopCoor(pos) {
   stopLatitude.innerHTML = pos.coords.latitude.toFixed(2);
   stopLongitude.innerHTML = pos.coords.longitude.toFixed(2);
+  saveToLocalStorage();
 }
 
 function loadFromLocalStorage() {
-  if (storageAvailable("localStorage")) {
-    startTime = Number(localStorage.getItem("startTime"));
-    historyTable.innerHTML = historyTable;
-    running = Number(localStorage.getItem('running'));
-    paused = Number(localStorage.getItem('paused'));
-    if (running) {
-      timeInterval = setInterval(getDisplayTime, 10);
-    }
+  startTime = Number(localStorage.getItem("startTime"));
+  historyTable.innerHTML = localStorage.historyTable;
+  timeDisplay.innerHTML = localStorage.timeDisplay;
+  running = Number(localStorage.getItem('running'));
+  paused = Number(localStorage.getItem('paused'));
+  tableSize = localStorage.tableSize;
+  currentTime = new Date().getTime(); 
+  
+  newRow = historyTable.rows[tableSize];
+  newStart = newRow.cells[0];
+  startLatitude = newRow.cells[1];
+  startLongitude = newRow.cells[2];  
+  newStop = newRow.cells[3];
+  stopLatitude = newRow.cells[4];
+  stopLongitude = newRow.cells[5];
+  elapsedTime = newRow.cells[6];
+  
+  if (running) {
+    timeInterval = setInterval(getDisplayTime, 10);
+  }
+  else if(paused && timeInterval){
+    clearInterval(timeInterval);
   }
 }
 
 function saveToLocalStorage() {
-  if (storageAvailable("localStorage")) {
-    localStorage.setItem("startTime", startTime);
-    localStorage.setItem("historyTable", historyTable);
-    localStorage.running = running;
-    localStorage.paused = paused;
-    localStorage.useStorage = 1;
-  }
+  localStorage.setItem("startTime", startTime);
+  localStorage.running = running;
+  localStorage.paused = paused;
+  localStorage.tableSize = tableSize;
+  localStorage.setItem("historyTable", historyTable.innerHTML);
+  localStorage.setItem("timeDisplay", timeDisplay.innerHTML);
+  localStorage.useStorage = 1;
 }
 
-if (
-  storageAvailable("localStorage") &&
-  Number(localStorage.getItem("useStorage"))
-) {
-  loadFromLocalStorage();
+if(localStorage.getItem("useStorage")) {
+  loadFromLocalStorage();  
 }
